c5817ffc097b2e79785db63067e3e4883d68b69b
jfadelhaye <julien.delhaye@portainer.io>
2022-03-23 15:23:06 +0100

    remove unnecessary sudo, update token file path to /tmp and add some text

--- install_nomad.sh
+++ install_nomad.sh
@@ -1,7 +1,9 @@
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-apt-get update -y && sudo apt-get install -y nomad
+apt-get update -y && apt-get install -y nomad
 systemctl start nomad
+
+echo "enabling ACL ..."
 
 cat <<EOF >> /etc/nomad.d/nomad.hcl
 acl {
@@ -12,5 +14,6 @@ EOF
 systemctl restart nomad
 echo " gives 5 secondes to restart nomad... "
 sleep 5 
-nomad acl bootstrap | tee /home/ubuntu/my_acl_token
+nomad acl bootstrap | tee /tmp/my_acl_token
+echo "you can also find your acl token in /tmp/my_acl_token"
 
